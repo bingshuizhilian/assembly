d6f976e0dc6be3f1676736bef72a84b4e1f5aaf3
bingshuizhilian <bingshuizhilian@yeah.net>
2020-02-22 17:43:01 +0800

    Update 42.asm

--- src/42.asm
+++ src/42.asm
@@ -3,7 +3,15 @@
 ; 题目：安装一个新的int 7ch中断例程，实现通过逻辑扇区号对软盘进行读写
 
 ; 说明：笔者未找到/安装合适的软盘测试环境，所以源代码仅验证了逻辑，没有经过实际测试
-
+; 说明2：笔者于20200222已经搭建了测试环境，并验证了程序的正确性，所需环境及工具如下
+;        (1) 使用虚拟机VMware 15.5.1版本(笔者用之前安装旧的8.0版本不能正常测试本例程)，并安装msdos7.1
+;            1) 安装教程     ： https://jingyan.baidu.com/article/a3761b2ba0da561577f9aa76.html
+;            2) msdos7.1镜像 ： https://winworldpc.com/product/ms-dos/7x
+;            3) vmware15.5.1 ： http://www.carrotchou.blog/122.html
+;        (2) 安装好环境后，需要把masm5.0的相关文件夹和文件拷贝进msdos7.1的虚拟硬盘中
+;            1) masm5.0      ： https://github.com/bingshuizhilian/assembly/tree/master/tool
+;            2) 文件拷贝方法 ： https://blog.csdn.net/q939369729/article/details/78726875
+;        (3) 至此，所有环境及软件已经就绪，在msdos7.1中可以使用edit命令修改asm文件，masm及link可输入命令使用
 
 assume cs:code, ds:data, ss:stack
 
@@ -32,13 +40,13 @@ mov ds, ax
 ;       (3)用es:bx指向存储读出数据或写入数据的内存区
 
 ; int 7ch写入测试
-; push ds
-; pop es
-; xor bx, bx
-; mov ah, 1
-; mov dx, 1
-; int 7ch
-; call delay
+push ds
+pop es
+xor bx, bx
+mov ah, 1
+mov dx, 1
+int 7ch
+call delay
 
 ; int 7ch读取测试
 mov ax, 2000h
